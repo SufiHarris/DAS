451cdb7fd68ddb0578c336a201248773f4ffdeae
SufiHarris <harrissofi02@gmail.com>
2024-04-18 16:38:47 +0530

    Added side bar

---
+++ DAS/Shared/LogoAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  LogoAnimation.swift
+//  DAS
+//
+//  Created by Mohammad Haris Sofi on 17/04/24.
+//
+
+import SwiftUI
+
+struct LogoAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoAnimation()
+}
